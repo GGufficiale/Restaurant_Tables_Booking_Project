ff4dd509c7b0828c5e93eb8e489694531f910b21
GGufficiale <germangranovskii@mail.ru>
2024-10-28 16:35:55 +0200

    корректировка приложения restaurant перед миграцией

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
 from django.views.decorators.cache import cache_page
 
-from restaurant.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, \
-    toggle_activity, ContactPageView
-from restaurant.apps import MainConfig
+from restaurant.views import BookingListView, BookingDetailView, BookingCreateView, BookingUpdateView, \
+    BookingDeleteView, ContactPageView
+from restaurant.apps import RestaurantConfig
 
-app_name = MainConfig.name
+app_name = RestaurantConfig.name
 urlpatterns = [
-    path('', ProductListView.as_view(), name='products_list'),
+    path('', BookingListView.as_view(), name='booking_list'),
     # метод для отображения отдельной страницы с товаром (по одному товару)
     path('restaurant/contact/', ContactPageView.as_view(), name='contact'),
     # В декоратор кеширования cache_page передается время его жизни (60 сек) и ссылка на контроллер (contact).
     # Это позволяет кешировать весь контроллер
-    path('restaurant/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='products_detail'),
-    path('restaurant/create', ProductCreateView.as_view(), name='products_create'),
-    path('restaurant/<int:pk>/update/', ProductUpdateView.as_view(), name='products_update'),
-    path('restaurant/<int:pk>/delete/', ProductDeleteView.as_view(), name='products_delete'),
-    path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
+    path('restaurant/<int:pk>/', cache_page(60)(BookingDetailView.as_view()), name='booking_detail'),
+    path('restaurant/create', BookingCreateView.as_view(), name='booking_create'),
+    path('restaurant/<int:pk>/update/', BookingUpdateView.as_view(), name='booking_update'),
+    path('restaurant/<int:pk>/delete/', BookingDeleteView.as_view(), name='booking_delete'),
+    # path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
 ]
