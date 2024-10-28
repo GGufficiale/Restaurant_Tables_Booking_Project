b21d7e561328dd76032a92e6982ac294178cdb61
GGufficiale <germangranovskii@mail.ru>
2024-10-28 15:57:12 +0200

    корректировка моделей

--- restaurant/models.py
+++ restaurant/models.py
@@ -6,6 +6,21 @@ from users.models import User
 NULLABLE = {'blank': True, 'null': True}  # форма, если параметр необязателен
 
 
+class Table(models.Model):
+    number = models.IntegerField(max_length=100, verbose_name='№ стола', help_text='Укажите № стола')
+    description = models.CharField(max_length=1000, verbose_name='Описание стола', help_text='Укажите тип стола',
+                                   **NULLABLE)
+    seats = models.IntegerField(max_length=20, verbose_name='К-во мест', help_text='Укажите к-во мест', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.number}: {self.description}, {self.seats}'
+
+    class Meta:
+        verbose_name = 'стол'
+        verbose_name_plural = 'столы'
+        ordering = ['number', 'description', 'seats']
+
+
 class Booking(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя гостя', help_text="Введите имя или имэйл")
     description = models.CharField(max_length=1000, verbose_name='Пожелания',
@@ -15,39 +30,22 @@ class Booking(models.Model):
     photo = models.ImageField(upload_to='catalog/photo', verbose_name="Фото",
                               help_text="Загрузите скрин из ваших соцсетей для получения скидки", **NULLABLE)
     # Для работы с изображениями в Джанго надо установить пакет Pillow"""
-    created_at = models.DateField(**NULLABLE, verbose_name='дата создания брони в БД', auto_now_add=True)
-    updated_at = models.DateField(**NULLABLE, verbose_name='дата последнего изменения брони в БД', auto_now=True)
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     slug = models.CharField(max_length=150, verbose_name='slug', null=True, blank=True)
-    owner = models.ForeignKey(User, verbose_name='владелец товара', help_text='Укажите владельца товара', **NULLABLE,
+    owner = models.ForeignKey(User, verbose_name='Юзер', help_text='Укажите создателя бронирования', **NULLABLE,
+                              on_delete=models.SET_NULL)
+    table = models.ForeignKey(Table, verbose_name='Стол', help_text='Укажите № стола', **NULLABLE,
                               on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f'{self.name}: {self.description}. Цена: {self.price}'
+        return f'{self.name}: {self.description}. Время:{self.time}. Стол: {self.table}'
 
     class Meta:
-        verbose_name = 'товар'
-        verbose_name_plural = 'товары'
-        ordering = ['name', 'description', 'price']
+        verbose_name = 'Бронирование'
+        verbose_name_plural = 'Бронирования'
+        ordering = ['name', 'description', 'time', 'table']
         # Поля для функционала прав доступа - варианты редактирования
-        permissions = [
-            ('cancel_publication', 'Can cancel publication'),
-            ('edit_description', 'Can edit description'),
-            ('change_category', 'Can change category'),
-        ]
-
-
-class Table(models.Model):
-    number = models.IntegerField(max_length=100, verbose_name='№ стола')
-    description = models.CharField(max_length=1000, verbose_name='описание стола', **NULLABLE)
-    owner = models.ForeignKey(User, verbose_name='гость, забронировавший стол',
-                              help_text='Укажите гостя, забронировавшего стол', **NULLABLE,
-                              on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return f'{self.number}: {self.description}, {self.owner}'
-
-    class Meta:
-        verbose_name = 'стол'
-        verbose_name_plural = 'столы'
-        ordering = ['name', 'description', 'owner']
+        # permissions = [
+        #     ('cancel_publication', 'Can cancel publication'),
+        #     ('edit_description', 'Can edit description'),
+        #     ('change_category', 'Can change category'),
+        # ]
